d4a6767a3fffedc0dcdb5a9b036f0782cb4f2a17
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-02-01 16:25:05 +0200

    hamburger menu

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 console.log("working");
 const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+const xMark = document.querySelector(".fa-xmark");
 document.querySelector(".hamburger").addEventListener("click", function () {
-  hamburger.classList.toggle("is-active");
+  console.log("working");
+  mobileNav.classList.add("is-active");
+});
+document;
+xMark.addEventListener("click", function () {
+  mobileNav.classList.remove("is-active");
 });
