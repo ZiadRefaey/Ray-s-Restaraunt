2c44cf09bb6f24bb9422d3014a9411a0378b9e12
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-02-02 20:39:53 +0200

    slider and abou sections

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-console.log("working");
 const hamburger = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".mobile-nav");
 const xMark = document.querySelector(".fa-xmark");
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    760: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 90,
+    },
+  },
+});
 document.querySelector(".hamburger").addEventListener("click", function () {
   console.log("working");
   mobileNav.classList.add("is-active");
 });
-document;
 xMark.addEventListener("click", function () {
   mobileNav.classList.remove("is-active");
 });
