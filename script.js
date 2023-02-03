bd285b6288039da5b126a446ccc8c81fb41fc3aa
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-02-03 03:07:24 +0200

    Finished without responsive

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   loop: true,
   spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -26,4 +29,25 @@ document.querySelector(".hamburger").addEventListener("click", function () {
 });
 xMark.addEventListener("click", function () {
   mobileNav.classList.remove("is-active");
+});
+const tabs = document.querySelectorAll(".reviews-tab");
+const tabContainer = document.querySelector(".reviews-tabs__container");
+const contentContainer = document.querySelector;
+const reviewContent = document.querySelectorAll(".reviews-content");
+tabContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".reviews-tab");
+  if (!clicked) return;
+  tabs.forEach((t) => {
+    t.classList.remove("reviews-tab__active");
+  });
+  clicked.classList.add("reviews-tab__active");
+  const datatab = clicked.dataset.tab;
+  reviewContent.forEach((r) => {
+    r.classList.remove("reviews-content__active");
+  });
+  const variable = document.querySelector(`.reviews-content__${datatab}`);
+  console.log(variable);
+  document
+    .querySelector(`.reviews-content__${datatab}`)
+    .classList.add("reviews-content__active");
 });
