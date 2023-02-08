ae98b100b2d2084b803befaf8c35eceed0b36683
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-02-08 18:03:17 +0200

    sticky navbar feature

---
+++ animationsScript.js
@@ -0,0 +1,38 @@
+const header = document.querySelector(".header");
+const nav = document.querySelector(".navbar");
+const sections = document.querySelectorAll(".section");
+function obsCallBack(enteries, observerHeader) {
+  const [entery] = enteries;
+  if (!entery.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+const obsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: "-100px",
+};
+
+// const observerHeader = new IntersectionObserver(obsCallBack, obsOptions);
+// observerHeader.observe(header);
+
+// function obsSectionsCallback(enteries, observer) {
+//   const [entery] = enteries;
+//   console.log(entery);
+//   if (entery.isIntersecting) {
+//     entery.target.classList.remove("hidden-section");
+//   } else {
+//     entery.target.classList.add("hidden-section");
+//   }
+// }
+
+// const sectionObserver = new IntersectionObserver(obsSectionsCallback, {
+//   root: null,
+//   threshold: 0.05,
+// });
+// sections.forEach((section) => {
+//   sectionObserver.observe(section);
+//   section.classList.add("hidden-section");
+// });
